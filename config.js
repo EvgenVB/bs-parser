862679c5ec1c9174aa80c9bc5d4c00d4fd8799f8
Evgeniy Bazhenov <evbazhenov@neotech.ee>
2017-02-15 15:21:25 +0300

    Switch wh

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ module.exports = {
         },
         android: {
             "1.0.0": {
-                redirect: false,
+                redirect: true,
                 getRedirectData: function(cb) {
                     getLeonOffshorePromoLink('72753421', function(err, link) {
                         cb(err, getBaseRedirectData(link));
