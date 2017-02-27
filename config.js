fabfd8354888e8e1ecd7b470ab8909ae70020910
Evgeniy Bazhenov <evbazhenov@neotech.ee>
2017-02-27 11:03:46 +0300

    change leon promo

--- config.js
+++ config.js
@@ -141,7 +141,7 @@ function getBaseRedirectData(url) {
 }
 
 function getLeonOffshorePromoLink(sub, cb) {
-    cb(null, `https://lblandingslink2.com/aff/ln/ru/${sub}`);
+    cb(null, `https://lblandsslinks7.xyz/aff/ln/ru/${sub}`);
     return;
     redis.get('mirror', function (err, data) {
         console.log(data);
