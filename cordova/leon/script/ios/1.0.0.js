341e19b43c6fa2ea1636fe30a05044a4d3fc951c
Evgeniy Bazhenov <evbazhenov@neotech.ee>
2017-03-14 15:20:11 +0300

    update script

--- cordova/leon/script/ios/1.0.0.js
+++ cordova/leon/script/ios/1.0.0.js
@@ -13,7 +13,9 @@ var backButtonStyles = {
 };
 
 var knownHosts = [
-    { host: /http:\/\/[a-zA-Z0-9.]+gameassists\.co\.uk.*/gi , style: 'absolute-small', args: [0, 0] }
+    { host: /http:\/\/[a-zA-Z0-9-.]+gameassists\.co\.uk.*/gi , style: 'absolute-small', args: [0, 0] },
+    { host: /https:\/\/[a-zA-Z0-9-.]+betsoftgaming\.com.*/gi , style: 'absolute-small', args: ['90%', 0] }
+    { host: /https:\/\/[a-zA-Z0-9-.]+casinomodule\.com.*/gi , style: 'absolute-small', args: [0, 0] }
 ];
 
 function getButtonStyle() {
